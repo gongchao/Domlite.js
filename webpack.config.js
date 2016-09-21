4a8902e1b872468f0bf2f1fc3b8f35d9e762486e
gongchao <ifeng1993@gmial.com>
2016-09-21 14:20:45 +0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 
 const webpack = require('webpack')
 
-module.exprots = {
-    
+module.exports = {
+    watch: true,
+    entry: {
+        index: './src/domlite.js'
+    },
+    output: {
+        path: './dist/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
 }
